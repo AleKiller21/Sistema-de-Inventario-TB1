c53efd7b22655fcb5bfd0cc75fef26f780c00f68
unknown <aleferrera@hotmail.com>
2015-12-06 11:25:13 -0600

    Eliminacion de botones del menu

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
-            this.btnIngreso = new System.Windows.Forms.Button();
-            this.btnInventario = new System.Windows.Forms.Button();
-            this.btnReportes = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
             this.holaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -45,81 +41,12 @@
             this.toolStripDropDownButton4 = new System.Windows.Forms.ToolStripDropDownButton();
             this.toolStripDropDownButton5 = new System.Windows.Forms.ToolStripDropDownButton();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDropDownButton6 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.existenciaDeProductosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.puntoDeReordenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ventasPorMesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnIngreso
-            // 
-            this.btnIngreso.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.btnIngreso.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnIngreso.FlatAppearance.BorderColor = System.Drawing.Color.DarkCyan;
-            this.btnIngreso.FlatAppearance.BorderSize = 2;
-            this.btnIngreso.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
-            this.btnIngreso.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
-            this.btnIngreso.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIngreso.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIngreso.ForeColor = System.Drawing.Color.DarkBlue;
-            this.btnIngreso.Location = new System.Drawing.Point(12, 88);
-            this.btnIngreso.Name = "btnIngreso";
-            this.btnIngreso.Size = new System.Drawing.Size(286, 99);
-            this.btnIngreso.TabIndex = 0;
-            this.btnIngreso.Text = "Ingresar registro";
-            this.btnIngreso.UseVisualStyleBackColor = false;
-            // 
-            // btnInventario
-            // 
-            this.btnInventario.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.btnInventario.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnInventario.FlatAppearance.BorderColor = System.Drawing.Color.DarkCyan;
-            this.btnInventario.FlatAppearance.BorderSize = 2;
-            this.btnInventario.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
-            this.btnInventario.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Firebrick;
-            this.btnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnInventario.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInventario.ForeColor = System.Drawing.Color.DarkBlue;
-            this.btnInventario.Location = new System.Drawing.Point(12, 404);
-            this.btnInventario.Name = "btnInventario";
-            this.btnInventario.Size = new System.Drawing.Size(286, 99);
-            this.btnInventario.TabIndex = 1;
-            this.btnInventario.Text = "Inventarios";
-            this.btnInventario.UseVisualStyleBackColor = false;
-            this.btnInventario.Click += new System.EventHandler(this.btnInventario_Click);
-            // 
-            // btnReportes
-            // 
-            this.btnReportes.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.btnReportes.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnReportes.FlatAppearance.BorderColor = System.Drawing.Color.DarkCyan;
-            this.btnReportes.FlatAppearance.BorderSize = 2;
-            this.btnReportes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
-            this.btnReportes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold;
-            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnReportes.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReportes.ForeColor = System.Drawing.Color.DarkBlue;
-            this.btnReportes.Location = new System.Drawing.Point(655, 88);
-            this.btnReportes.Name = "btnReportes";
-            this.btnReportes.Size = new System.Drawing.Size(286, 99);
-            this.btnReportes.TabIndex = 2;
-            this.btnReportes.Text = "Reportes";
-            this.btnReportes.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.DarkCyan;
-            this.button3.FlatAppearance.BorderSize = 2;
-            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
-            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.DarkBlue;
-            this.button3.Location = new System.Drawing.Point(655, 404);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(286, 99);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Ignorar";
-            this.button3.UseVisualStyleBackColor = false;
             // 
             // toolStrip1
             // 
@@ -129,7 +56,8 @@
             this.toolStripDropDownButton3,
             this.toolStripDropDownButton4,
             this.toolStripDropDownButton5,
-            this.toolStripLabel1});
+            this.toolStripLabel1,
+            this.toolStripDropDownButton6});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(953, 25);
@@ -241,6 +169,39 @@
             this.toolStripLabel1.Size = new System.Drawing.Size(141, 22);
             this.toolStripLabel1.Text = "Resumen de Inventario";
             // 
+            // toolStripDropDownButton6
+            // 
+            this.toolStripDropDownButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton6.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.existenciaDeProductosToolStripMenuItem,
+            this.puntoDeReordenToolStripMenuItem,
+            this.ventasPorMesToolStripMenuItem});
+            this.toolStripDropDownButton6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.toolStripDropDownButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton6.Image")));
+            this.toolStripDropDownButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton6.Name = "toolStripDropDownButton6";
+            this.toolStripDropDownButton6.ShowDropDownArrow = false;
+            this.toolStripDropDownButton6.Size = new System.Drawing.Size(62, 22);
+            this.toolStripDropDownButton6.Text = "Reportes";
+            // 
+            // existenciaDeProductosToolStripMenuItem
+            // 
+            this.existenciaDeProductosToolStripMenuItem.Name = "existenciaDeProductosToolStripMenuItem";
+            this.existenciaDeProductosToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.existenciaDeProductosToolStripMenuItem.Text = "Existencia de Productos";
+            // 
+            // puntoDeReordenToolStripMenuItem
+            // 
+            this.puntoDeReordenToolStripMenuItem.Name = "puntoDeReordenToolStripMenuItem";
+            this.puntoDeReordenToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.puntoDeReordenToolStripMenuItem.Text = "Punto de Reorden";
+            // 
+            // ventasPorMesToolStripMenuItem
+            // 
+            this.ventasPorMesToolStripMenuItem.Name = "ventasPorMesToolStripMenuItem";
+            this.ventasPorMesToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.ventasPorMesToolStripMenuItem.Text = "Ventas por Mes";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -248,10 +209,6 @@
             this.BackColor = System.Drawing.Color.Snow;
             this.ClientSize = new System.Drawing.Size(953, 572);
             this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.btnReportes);
-            this.Controls.Add(this.btnInventario);
-            this.Controls.Add(this.btnIngreso);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -266,10 +223,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnIngreso;
-        private System.Windows.Forms.Button btnInventario;
-        private System.Windows.Forms.Button btnReportes;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
         private System.Windows.Forms.ToolStripMenuItem holaToolStripMenuItem;
@@ -282,5 +235,9 @@
         private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton4;
         private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton5;
         private System.Windows.Forms.ToolStripButton toolStripLabel1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton6;
+        private System.Windows.Forms.ToolStripMenuItem existenciaDeProductosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem puntoDeReordenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ventasPorMesToolStripMenuItem;
     }
 }
